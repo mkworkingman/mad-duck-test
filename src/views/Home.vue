8b77ec003cc122522ea91462e4e86b3b26274f79
Maksim Kalinin <mkworkingman@gmail.com>
2022-06-02 19:04:09 +0200

    [master] Add functionality toHome page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,38 @@
     <h1 class="main__heading">Add Cities</h1>
     <p class="main__description">Add 5 cities whose temperature you want to track.</p>
     <form @submit.prevent="submitCity" class="enter-city">
-      <input class="enter-city__btn enter-city__btn--plus" type="submit" value="+">
-      <input class="enter-city__input" type="text" placeholder="Add a city…">
-      <input class="enter-city__btn enter-city__btn--add" type="submit" value="Add">
+      <input
+        class="enter-city__btn enter-city__btn--plus"
+        type="submit"
+        value="+"
+      >
+      <input
+        class="enter-city__input"
+        type="text"
+        placeholder="Add a city…"
+        v-model="cityName"
+      >
+      <input
+        class="enter-city__btn enter-city__btn--add"
+        type="submit"
+        value="Add"
+      >
     </form>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 export default {
   name: 'Home',
   setup() {
+    const cityName = ref('')
+
     const submitCity = () => {
-      console.log('test')
+      console.log(cityName.value.trim())
     }
 
-    return { submitCity }
+    return { cityName, submitCity }
   },
 }
 </script>
@@ -33,15 +49,29 @@ export default {
   &__heading {
     font-size: 20px;
     margin-bottom: 10px;
+
+    @media (min-width: 820px) {
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
   }
 
   &__description {
     font-size: 14px;
+    font-weight: 300;
+
+    @media (min-width: 820px) {
+      font-size: 24px;
+    }
   }
 
   .enter-city {
     margin-top: 30px;
     position: relative;
+
+    @media (min-width: 820px) {
+      margin-top: 60px;
+    }
 
     &__input {
       width: 100%;
@@ -52,6 +82,11 @@ export default {
       border: 1px solid transparent;
       transition: border-color 250ms, box-shadow 250ms;
       outline: 0;
+
+      @media (min-width: 820px) {
+        font-size: 20px;
+        padding: 19px 130px 19px 80px;
+      }
 
       &:hover {
         border-color: #5abece;
@@ -77,6 +112,13 @@ export default {
         height: 22px;
         width: 22px;
         border-radius: 50%;
+
+        @media (min-width: 820px) {
+          height: 40px;
+          width: 40px;
+          font-size: 28px;
+          left: 20px;
+        }
       }
 
       &--add {
@@ -84,8 +126,15 @@ export default {
         right: 2px;
         border-radius: 10px;
         font-size: 14px;
-        line-height: 26px;
+        height: 26px;
         padding-inline: 12px;
+
+        @media (min-width: 820px) {
+          right: 8px;
+          font-size: 20px;
+          height: 54px;
+          padding-inline: 36px;
+        }
       }
     }
   }
