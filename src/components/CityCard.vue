e576a084c35eed0d11cbaaaa39071079e6c8c289
Maksim Kalinin <mkworkingman@gmail.com>
2022-06-03 11:07:15 +0200

    [master] Add request to weatherstack

--- src/components/CityCard.vue
+++ src/components/CityCard.vue
@@ -1,14 +1,27 @@
 <template>
-  <router-link :to="'/' + card" class="card">
+  <router-link :to="'/' + name" class="card">
     <h4>{{card}}</h4>
   </router-link>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import axios from 'axios'
 export default {
   props: ['card'],
-  setup() {
-    
+  setup(props) {
+    const name = ref(props.card.toLowerCase().split(' ').join('_'))
+
+    axios.get('http://api.weatherstack.com/current', {
+      params: {
+        access_key: '3c1bc2be7adb78435808e07c3cdb66c7',
+        query: name
+      }
+    }).then(res => {
+      console.log(res)
+      console.log(res.data)
+    })
+    return { name }
   },
 }
 </script>
