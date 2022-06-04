33e0b7c8fa78ca284874afc219cffb11ddad7c84
Maksim Kalinin <mkworkingman@gmail.com>
2022-06-05 00:00:07 +0200

    [master] Refactor City Tests

--- src/views/City.spec.js
+++ src/views/City.spec.js
@@ -3,39 +3,43 @@ import { render, screen, waitForElementToBeRemoved } from '@testing-library/vue'
 import "@testing-library/jest-dom"
 
 jest.mock('vue-router')
-jest.useFakeTimers()
+beforeEach(() => {
+  localStorage.setItem('cities', null)
+  require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
+});
 
 describe('City Page', () => {
   it('Check Formatted Name', () => {
-    require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
     render(City)
     const heading = screen.queryByRole('heading', { level: 1, name: 'Novi Sad' })
     expect(heading).toBeInTheDocument()
   })
 
   it('Not Included 1', () => {
-    require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
     const { container } = render(City)
     expect(container.querySelector('.main-info__status').innerHTML).toEqual('Sorry, this city is not included.')
   })
 
   it('Not Included 2', () => {
     localStorage.setItem('cities', '["astrakhan","sydney","podgorica"]')
-    require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
+    const { container } = render(City)
+    expect(container.querySelector('.main-info__status').innerHTML).toEqual('Sorry, this city is not included.')
+  })
+
+  it('Wrong Localstorage', () => {
+    localStorage.setItem('cities', 'test')
     const { container } = render(City)
     expect(container.querySelector('.main-info__status').innerHTML).toEqual('Sorry, this city is not included.')
   })
 
   it('Included', () => {
     localStorage.setItem('cities', '["astrakhan","sydney","novi_sad","podgorica"]')
-    require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
     const { container } = render(City)
     expect(container.querySelector('.main-info__status').innerHTML).toEqual('Loading...')
   })
 
   it('After Loading', async () => {
     localStorage.setItem('cities', '["astrakhan","sydney","novi_sad","podgorica"]')
-    require('vue-router').useRoute.mockReturnValueOnce({ params: { city: 'novi_sad' } })
     const { container } = render(City)
     await waitForElementToBeRemoved(screen.getByText('Loading...'))
       .then(() => {
